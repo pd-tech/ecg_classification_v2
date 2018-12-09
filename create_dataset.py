a893ea7fe4de16750ea96583a00cc4b920d16051
pd-tech <33969922+pd-tech@users.noreply.github.com>
2018-12-09 22:31:31 +0100

    Major update with support for gen_tiles.py

--- create_dataset.py
+++ create_dataset.py
@@ -13,20 +13,59 @@ def convert(value, typ):
         return int(value)
     if "s" in typ:  #seconds
         return int(value)*freq
+    if "start" in value or "end" in value:
+        return value
+    return int(value)
 
-def parse(entry):
+def preprocess(entry, shift):
     spoint = entry[0].split(" ")
-    epoint = entry[1].split(" ")
-    typ = spoint[1]
+    epoint = entry[1+shift].split(" ")
+    if(len(spoint) > 1): typ = spoint[1]
+    else: typ = "vzorek"
     operand = epoint[0]
 
+    return spoint, epoint, typ, operand
+
+def enumerate(spoint, epoint, step, src):
+    start = spoint
+    slist = []
+    elist = []
+
+    while(start < epoint):
+        if((start + step) <= epoint):
+            slist.append(start)
+            elist.append(start + step)
+        if((start + step) > epoint):
+            slist.append(start)
+            elist.append(epoint)
+            break
+        start += step
+
+    return slist, elist
+
+
+def parse(entry, src_file):
+    pos_shift = 0
+    if(len(entry) == 1 and "-" in entry[0]): entry = entry[0].replace("-", " - ").split("-")
+    if(len(entry) >= 3 and ">" in entry[2]): pos_shift += 1
+
+    spoint, epoint, typ, operand = preprocess(entry, pos_shift)
+
+    if "start" in spoint: spoint = "0 v".split(" ")
+    if "end" in epoint: epoint = (str(sum(1 for line in open(src_file) if line.rstrip())) + " v").split(" ")
+
     spoint = convert(spoint[0], typ)
-    if len(epoint) <= 2: epoint = convert(epoint[1], typ)
+    if(len(epoint) <= 2): epoint = convert(epoint[1-pos_shift], typ)
     else: epoint = convert(epoint[1], epoint[2])
     
     if operand == "+": return(spoint, spoint+epoint)
     if operand == "-": return(spoint-epoint, spoint)
     if operand == ".": return(spoint, epoint)
+    if operand == "►" or operand == ">":
+        step = convert(entry[1].split(" ")[0], entry[1].split(" ")[1])
+        return enumerate(spoint, epoint, step, src_file)
+
+    return(spoint, epoint)
 
 def csvreader(src_file, start_points, end_points):
     volts = []
@@ -48,6 +87,11 @@ def pointReader(point_file):
     end_points = [] 
     dataset = []
 
+    all_spoint = []
+    all_epoint = []
+
+    file_sep = [0]
+
     print("Turning pointer list into dataset...")
 
     with open(point_file) as pfile:
@@ -58,17 +102,26 @@ def pointReader(point_file):
                 if not src_file: src_file = entry[0].split('"')[1]
                 else:
                     dataset.extend(csvreader(src_file, start_points, end_points))
+                    file_sep.append(len(start_points))
+                    all_spoint.extend(start_points)
+                    all_epoint.extend(end_points)
                     start_points = []
                     end_points = []
-                    src_file = entry.split('"')[1]
+                    src_file = entry[0].split('"')[1]
                 continue
-            spoint, epoint = parse(entry)
+            spoint, epoint = parse(entry, src_file)
 
-            start_points.append(spoint)
-            end_points.append(epoint)
+            if isinstance(spoint, list):
+                start_points.extend(spoint)
+                end_points.extend(epoint)
+            else:
+                start_points.append(spoint)
+                end_points.append(epoint)
 
+        all_spoint.extend(start_points)
+        all_epoint.extend(end_points)
         dataset.extend(csvreader(src_file, start_points, end_points))
-        return dataset
+        return dataset, start_points, end_points, all_spoint, all_epoint, file_sep
 
 def save(dataset, label):
     num = 0
